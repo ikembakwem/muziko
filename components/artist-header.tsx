28cbe7d2725f3328deacb209277264cc7310e80d
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-10 08:51:33 +0100

    Configure next to treat SVG files as React components using @svgr/webpack, modify TypeScript absolute path, implement ArtistHeader component

--- components/artist-header.tsx
+++ components/artist-header.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
+import PlayIcon from "@assets/icons/play.svg";
+import MenuIcon from "@assets/icons/menu.svg";
+
 export const ArtistHeader = () => {
   return (
     <div>
@@ -10,12 +13,20 @@ export const ArtistHeader = () => {
             className="h-full object-cover"
           />
         </div>
-        <div className="flex flex-col justify-between min-h-[324px] h-full w-full p-6 pt-5">
-          <div>header</div>
+        <div className="flex flex-col text-[#FA586A] justify-between min-h-[324px] h-full w-full p-6 pt-5">
+          <div className="flex justify-end">
+            <button className="cursor-pointer rounded-full">
+              <span>
+                <MenuIcon className="h-7 w-7" />
+              </span>
+            </button>
+          </div>
           <div className="flex justify-between">
-            <h1 className="text-4xl font-bold">Burna Boy</h1>
-            <button>
-              <span>btn</span>
+            <h1 className="text-4xl font-bold text-white">Burna Boy</h1>
+            <button className="cursor-pointer rounded-full">
+              <span>
+                <PlayIcon className="h-10 w-10" />
+              </span>
             </button>
           </div>
         </div>
